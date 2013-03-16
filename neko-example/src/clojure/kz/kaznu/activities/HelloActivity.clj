968b228083754f2455c5be31ed7bacfe7b27e3de
fallen <fallen.s4e@gmail.com>
2013-03-16 17:21:54 +0600

    checkboxes added: for remote evaluation and for compilation

--- neko-example/src/clojure/kz/kaznu/activities/HelloActivity.clj
+++ neko-example/src/clojure/kz/kaznu/activities/HelloActivity.clj
@@ -6,11 +6,13 @@
   (:use kz.kaznu.base.client)
   (:use kz.kaznu.base.utils)
   (:use kz.kaznu.base.serialization)
+  (:use kz.kaznu.service.server)
   (:gen-class :main false
               :extends android.app.Activity
               :exposes-methods {onCreate superOnCreate}))
 
 (def max-result-len 150)
+(def this-atom (atom nil))
 
 ;; components accessing
 
@@ -34,7 +36,28 @@
                            []
                          (onClick [#^android.view.View view] (fun view)))))
 
+(defn remote-checkbox-checked? [ ]
+  (.isChecked (.findViewById @this-atom R$id/remoteCheckbox)))
+
+(defn compiler-checkbox-checked? [ ]
+  (.isChecked (.findViewById @this-atom R$id/compilerCheckbox)))
+
 ;; eval methods
+
+(defn interprete
+  "expr is a parsed expression and the function returns evaluated expression. Cut version of eval"
+  [ input ]
+  (if (list? input)
+    (let [fun   (eval (first input)) ;; first elem in a list must be a symbol
+          args  (map #'interprete (rest input))]
+      (apply fun args))
+    (eval input)))
+  
+
+(defn eval-local [ expr ]
+  (if (compiler-checkbox-checked?)
+    (eval expr)
+    (interprete expr)))
 
 (defn eval-new-thread
   "evaluates expressions in new thread, catching all the errors being in this namespace kz.kaznu.activities.HelloActivity"
@@ -42,10 +65,10 @@
   @(future
      (binding [*ns* (find-ns 'kz.kaznu.activities.HelloActivity)]
        ((kz.kaznu.base.utils/catch-all apply)
-        eval exprs))))
+        eval-local exprs))))
 
 (defn eval-remote
-  "evaluates expressions in new thread, catching all the errors being in this namespace kz.kaznu.activities.HelloActivity"
+  "evaluates expressions remotely"
   [ expr input-addr ]
   @(future
      ((kz.kaznu.base.utils/catch-all kz.kaznu.base.client/runr)
@@ -60,9 +83,11 @@
                                        input-addr (.toString (.getText ui-input-address))]
                                    (try
                                      (set-result this "waiting ...")
-                                     (if (or (nil? input-addr) (= input-addr ""))
+                                     (if (not (remote-checkbox-checked?))
                                        (set-result this (str (eval-new-thread input)))
-                                       (set-result this (str (eval-remote input input-addr))))
+                                       (if (or (nil? input-addr) (= input-addr ""))
+                                         (set-result this "no address supplied")
+                                         (set-result this (str (eval-remote input input-addr)))))
                                      (catch Exception ex
                                        (set-result this (str "error occured:" ex))))))))
   (set-on-click ui-clear-button (fn[_]
@@ -72,6 +97,7 @@
 ;; overridings
 (defn -onCreate
   [this bundle]
+  (swap! this-atom (fn[_]this))
   (doto this
     (.superOnCreate bundle)
     (.setContentView R$layout/main))
--- neko-example/src/clojure/kz/kaznu/base/serialization.clj
+++
@@ -1,9 +0,0 @@
-(ns kz.kaznu.base.serialization)
-
-(defn seri[x]
-  (if (instance? String x)
-    (str "\"" x "\"")
-    (str x)))
-
-(defn deseri[x]
-  (read-string x))
